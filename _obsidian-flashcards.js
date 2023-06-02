6d1f3640629537fbb7c4c6f3becc86422541ac45
ian <me@iany.me>
2023-06-02 20:24:57 +0800

    update flashcards css

--- _obsidian-flashcards.js
+++ _obsidian-flashcards.js
@@ -1,13 +1,22 @@
 (function () {
   const tagEl = document.querySelector(".tags");
-  tagEl.innerHTML = tagEl.innerHTML
-    .split(" ")
-    .map((tag) => '<span class="tag">' + tag + "</span>")
-    .join(" ");
+  if (tagEl !== null) {
+    tagEl.innerHTML = tagEl.innerHTML
+      .split(" ")
+      .map((tag) => '<span class="tag">' + tag + "</span>")
+      .join(" ");
+  }
 
   const textEl = document.querySelector("p");
-  textEl.innerHTML = textEl.innerHTML.replace(
-    / › /g,
-    ' <span class="context-separator">›</span> '
-  );
+  if (textEl !== null) {
+    const parts = textEl.innerHTML.split(" › ");
+    if (parts.length > 1) {
+      textEl.innerHTML =
+        parts
+          .slice(0, parts.length - 1)
+          .join(' <span class="context-separator">›</span> ') +
+        ' <span class="context-separator last">›</span> ' +
+        parts[parts.length - 1];
+    }
+  }
 })();
